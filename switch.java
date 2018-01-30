e2c9a7647116159eefcb01d291ae14941dd73cb6
swamx <swamx@users.noreply.github.com>
2018-01-30 16:22:11 -0500

    Create switch.java

---
+++ switch.java
@@ -0,0 +1,19 @@
+int mcharge(int no_of_tran){
+if(no_of_tran>=30){
+	return 1;
+}
+else{
+	return 2;
+}
+}
+
+int mcharge_val;
+int no_transaction=45
+switch(mcharge(no_transcaction)){
+case 1:
+	mcharge_val=100;
+	break;
+default:
+mcharge_val =10;
+break;
+}
